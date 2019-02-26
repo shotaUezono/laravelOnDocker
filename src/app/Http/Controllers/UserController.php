85062e83c7e5519aefa9ae6f8d8719b683e76f96
shotaUezono <s.uezono.itc@gmail.com>
2019-02-26 12:23:00 +0900

    first commit

---
+++ src/app/Http/Controllers/UserController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\User;
+class UserController extends Controller
+{
+    public function index()
+    {
+        // データの追加 emailの値はランダムな文字列を使用。「.」で文字列の連結
+        $email = substr(str_shuffle('abcdefghijklmnopqrstuvwxyz'), 0, 8) . '@yyyy.com';
+        User::insert(['name' => 'yamada taro', 'email' => $email, 'password' => 'xxxxxxxx']);
+        // 全データの取り出し
+        $users = User::all();
+        return view('user', ['users' => $users]);    
+    }
+}
+
+/*
+namespace App\Http\Controllers;
+class UserController extends Controller
+{
+    public function index()
+    {
+        // ただの変数定義ですが、ここでModelにデータの処理を行わせたりします（後述）。
+        $name = 'yamada taro';
+
+        // ここでuserビュー「user.blade.php」を呼び出し、データ「name」を渡している。
+        return view('user', ['name' => $name]);
+    }
+}
+*/
